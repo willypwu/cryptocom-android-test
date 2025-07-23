9c2a83c17dd63cf80bc426bc6c2ed7d5c227fff0
Willy Wu <willywu@WillyMacBook-Air.local>
2025-07-23 11:19:56 +0800

    [Init]add basic package/UI structure

---
+++ app/src/main/java/com/test/cryptocom/presentation/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.test.cryptocom.presentation.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.test.cryptocom.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/test/cryptocom/presentation/ui/wallet/WalletFragment.kt
@@ -0,0 +1,26 @@
+package com.test.cryptocom.presentation.ui.wallet
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.test.cryptocom.databinding.FragmentWalletBinding
+
+class WalletFragment : Fragment() {
+
+    private var _binding: FragmentWalletBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentWalletBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
